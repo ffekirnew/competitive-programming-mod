6752ff501a7fd67104aa74d4e44aeba555d04685
ffekirnew <ffekirnew0808@gmail.com>
2022-10-11 19:39:31 +0300

    Solution to 1838. Frequency of the Most Frequent Element at LeetCode

---
+++ two-pointers-and-sliding-window/1838. Frequency of the Most Frequent Element.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        # sort nums
+        nums.sort()
+        
+        ### BRUTE FORCE ###
+        # answer = 0
+        # for i in range(1, len(nums)):
+        # 	j, curr = i - 1, k
+        # 	while j > -1 and curr >= nums[i] - nums[j]:
+        # 		curr -= nums[i] - nums[j]
+        # 		j -= 1
+        # 	answer = max(answer, i - j)
+        # return answer
+
+        # create the object to be returned
+        answer = 0
+        # sort the numbers
+        nums.sort()
+        # set up the sliding window
+        s = 0
+        e = 0
+        win_len = 0
+        win_sum = 0
+        # loop through nums
+        while e < len(nums):
+            win_sum += nums[e]
+            win_len += 1
+            while (nums[e] * win_len > win_sum + k):
+                win_sum -= nums[s]
+                win_len -= 1
+                s += 1
+            answer = max(answer, win_len)
+            e += 1
+        # return the solution
+        return answer
